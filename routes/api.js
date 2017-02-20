3e557af9832f5c6e7c17efd866cc681fdf3b28a1
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@dsl-hkibrasgw1-58c3f5-34.dhcp.inet.fi>
2017-02-21 00:47:01 +0200

    Ajout du nom d'entreprise en header, modification de l'API. Ajout d'une ouverture de fenêtre modale lors d'un click sur une image sur le scrolling Popup images block.

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var fileJs = fs.readFileSync('./api/client.js');
-
-var obj = {
-  company: "Our company",
-  webpage: "http://localhost:3000",
-  form: "http://localhost:3000"
-};
+var fileJs = fs.readFileSync('./api/js/client.js');
+var db = require('../api/service/db.js');
 
 /* GET home page. */
 router.get('/js', function(req, res, next) {
-  res.setHeader('content-type', 'text/javascript');
+  //res.setHeader('content-type', 'text/javascript');
   res.write(fileJs);
   res.end();
 })
-.get('/', function(req, res, next){
-  res.send(JSON.stringify(obj));
-  console.log("ENVOYE");
+.get('/company', function(req, res, next){
+  res.send(JSON.stringify(db.getCompany()));
 });
 
 module.exports = router;
