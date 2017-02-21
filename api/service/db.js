aaddde7d6717115cb481432aed0381e179a8489d
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@MacBook-Pro-de-Frederic.local>
2017-02-21 16:49:01 +0200

    Modification de la DB côté serveur, client récupère les données pour remplir une modale. Fenêtre modale pour chaque bouton customisée. Modification du CSS de la scrollingPopup vue.

--- api/service/db.js
+++ api/service/db.js
@@ -2,13 +2,53 @@
  * Created by fredericlopesgoncalvesmagalhaes on 21.02.17.
  */
 
+var data = [
+  {
+    id: 0,
+    name: "elec_worker",
+    header: "Dedicated to excellence",
+    desc: "At DELONIFR, our team is dedicated to excellence and we do our best to provide the best service to each customer." +
+    "We know how difficult it is to give trust and that is the reason why you will do our best for you."
+  },
+  {
+    id: 1,
+    name: "elec_outside",
+    header: "Feeling home outside like inside",
+    desc: "Brightning your outside is as much important as the inside of your home. Bringing light during night will give and secure your outside part of your home."
+  },
+  {
+    id: 2,
+    name: "elec_panels",
+    header: "Renewable energy",
+    desc: "At DELONIFR, we think that renewable energy is the future. Then, we offer a customized service to transform your house into an electricity generator to light up your home."
+  },
+  {
+    id: 3,
+    name: "elec_luster",
+    header: "High ceilling should be dressed as well",
+    desc: "With a simple luster."
+  },
+]
+var company = {
+  company: "DELONIFR",
+  creation: "2017",
+  webpage: "http://localhost:3000/"
+}
+
+function getDescObj(name){
+  for(var i=0; i<data.length; i++){
+    if(data[i].name === name){return data[i]};
+  }
+  return null;
+}
+
 module.exports = {
   getCompany: function () {
-    var obj = {
-      company: "Design & Cie",
-      creation: "2017",
-      webpage: "http://localhost:3000/"
-    }
-    return obj;
+    return company;
+  },
+
+  getDescription: function(id){
+    var data = getDescObj(id);
+    return data;
   }
 }
