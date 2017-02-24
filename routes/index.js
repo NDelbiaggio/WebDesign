b7490dbaa894bbc6df92a9727d1c864bec16e611
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@dsl-hkibrasgw1-58c3f5-34.dhcp.inet.fi>
2017-02-25 00:18:46 +0200

    Ajout de boutons pour le changement de la langue de la page. Ajout d'un effet fondu entre la page principale et la section suivante avec un gradient.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyCompany' });
+  res.render('index', { title: 'DELONIFR' });
+})
+.get('/french', function(req, res, next) {
+  res.render('french/index', {title: 'DELONIFRENCH'});
 });
 
 module.exports = router;
