81472569d3cdbe112bb6c2d0fe8bf9604c0a8c99
Delbiaggio Nicolas <delbiaggionicolas@dsl-hkibrasgw1-50df7b-244.dhcp.inet.fi>
2017-02-25 15:02:45 +0200

    delay before smooth when interruptor is clicked

--- public/javascripts/smoothScrolling.js
+++ public/javascripts/smoothScrolling.js
@@ -3,26 +3,29 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     // Add smooth scrolling to all links in navbar + footer link
-    $("#btnInterruptor a[href='#aboutUs'], .up-arrow, .navbar a, footer a[href='#mainPage']").on('click', function (event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 900, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
+    $(".up-arrow, .navbar a, footer a[href='#mainPage']").on('click', function (event) {
+        applySmooth(this, event);
     });
 })
+
+function applySmooth(caller, event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (caller.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = caller.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function () {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    }
+}
+
