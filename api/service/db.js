12745362f236e9eb1e2188bce85b0a109d07675a
Delbiaggio Nicolas <delbiaggionicolas@dsl-hkibrasgw1-50df7b-244.dhcp.inet.fi>
2017-02-28 01:28:23 +0200

    Finalisation for the presentation

--- api/service/db.js
+++ api/service/db.js
@@ -34,6 +34,13 @@ var data = [
         desc: "With a simple luster.",
         imgPath: 'images/scrollingPopupImages/elec_lustre.jpg'
     },
+    {
+        id: 4,
+        name: "plaquetry_room",
+        header: "Wood Wall",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        imgPath: '/images/scrollingPopupImages/plaquetry_room.jpg'
+    }
 ]
 var company = {
     company: "DELONIFR",
